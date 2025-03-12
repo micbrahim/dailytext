e5840c6e6d8bbf1c94455e5e3834f77c0a3291d6
micibr <micibr@cs.washington.edu>
2025-03-11 23:35:46 -0700

    init

---
+++ scraper.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import sys
+
+def wikipedia():
+    URL = "https://en.wikipedia.org/wiki/Main_Page"
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    for s in str(soup.b).split():
+        if s.startswith('href='):
+            link = 'https://en.wikipedia.org/' + s[6:-1]
+
+    main_content = soup.find("div", {"class": "mw-body-content"})
+
+    daily = main_content.find_all("p")[0].get_text()
+    return f'Daily article: {daily}' + '\n' + f"Find more at {link}"
+
+def send_imessage(phone_number, message_body):
+    os.system('osascript msg.applescript {} "{}"'.format( phone_number, message_body ) )
+
+def main():
+    number = int(sys.argv[1])
+    if not (isinstance(number, int)):
+        print("invalid number")
+        exit(1)
+    send_imessage(number, wikipedia())
+    print("message sent")
+
+if __name__ == "__main__":
+    main()
+
